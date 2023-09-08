a218a3d0fb1c6f1a7809399b956fa3237dd33c9e
amanda-benites <amandabenites2005@gmail.com>
2023-09-08 08:50:42 -0300

    Criando a base da página de criar publicação

--- src/page/create_post/CreatePost.js
+++ src/page/create_post/CreatePost.js
@@ -1,22 +1,38 @@
+import { useNavigate } from "react-router";
 import ScreenHeader from "../../components/sreen_header/ScreenHeader";
+import { ButtonCancel, ButtonOk, DivButtonsContainer, DivInputFile, DivInputsContainer, InputFileContainer, InputLegendContainer, SpanInsertPost } from "./style";
 
 
 function CreatePost() {
+
+  const navigate = useNavigate()
+
+  function goToHomePage() {
+      navigate("/home");
+  }
+
+  
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+  };
+
+  
     return(
         <>
           <ScreenHeader titlePage={"Criar Publicação"}/>  
-          <div>
-                <span>Inserir imagem</span>
-                <div>
-                    <input type="file"/>
-                </div>
-                <span>Inserir legenda</span>
-                <input type="text"/>
-          </div>
-          <div>
-            <button>Cancelar</button>
-            <button>Concluir</button>
-          </div>
+          <DivInputsContainer>
+                <SpanInsertPost>Inserir imagem</SpanInsertPost>
+                <DivInputFile>
+                    <input type="file" id="fileInput" style={{ display: 'none' }} onChange={handleFileChange} />
+                    <InputFileContainer onClick={() => document.getElementById('fileInput').click()}>Coloque a imagem aqui</InputFileContainer>
+                </DivInputFile>
+                <SpanInsertPost>Inserir legenda</SpanInsertPost>
+                <InputLegendContainer type="text"/>
+          </DivInputsContainer>
+          <DivButtonsContainer>
+            <ButtonCancel onClick={goToHomePage}>Cancelar</ButtonCancel>
+            <ButtonOk>Concluir</ButtonOk>
+          </DivButtonsContainer>
         </>
     )
 }

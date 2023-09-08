0c49bd9f47ad083a98397a5e293ff1c9020c7e09
amanda-benites <amandabenites2005@gmail.com>
2023-09-08 08:58:53 -0300

    Removendo arquivos desnecessários

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import StyledGlobal, { TelaInteira } from "./styleGlobal";
 
-// import Cover from "./page/cover/Cover"
-// import Auth from "./page/auth/Auth"
-// import CreateAccount from "./page/create_account/CreateAccount";
-// import Home from "./page/home/Home"
-// import Contacts from "./page/contacts/Contacts";
-// import Profile from "./page/profile/Profile";
-// import CreatePost from "./page/create_post/CreatePost";
 import RoutesPage from "./RoutesPage";
 
 function App() {
